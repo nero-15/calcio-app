a4aae54dd14096986c5202b68f740afdcd20afe6
nero-15 <nero15.dev@gmail.com>
2021-12-28 22:11:00 +0900

    setting token

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"net/url"
 	"path"
+
+	"github.com/nero-15/calcio-app/config"
 )
 
 func main() {
@@ -14,7 +16,7 @@ func main() {
 	url.Path = path.Join(url.Path, "teams", "108")
 
 	req, _ := http.NewRequest("GET", url.String(), nil)
-	req.Header.Add("X-Auth-Token", "Your API token") // アカウント登録時に送られてきたAPIトークンをリクエストヘッダーに追加
+	req.Header.Add("X-Auth-Token", config.Config.FootballDataApiToken) // アカウント登録時に送られてきたAPIトークンをリクエストヘッダーに追加
 	client := new(http.Client)
 	resp, _ := client.Do(req)
 	defer resp.Body.Close()
