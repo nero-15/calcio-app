8aeb6e742a76c507b054b4d854f7e2dc2363b174
nero-15 <nero15.dev@gmail.com>
2021-12-29 21:22:06 +0900

    setting echo

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -1,26 +1,62 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"path"
 
+	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/nero-15/calcio-app/config"
 )
 
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
-	// 取得したいデータのURL作成
-	url, _ := url.Parse(config.Config.FootballDataBaseUrl)
-	url.Path = path.Join(url.Path, "teams", "108")
+	e := echo.New()
 
-	req, _ := http.NewRequest("GET", url.String(), nil)
-	req.Header.Add("X-Auth-Token", config.Config.FootballDataApiToken) // アカウント登録時に送られてきたAPIトークンをリクエストヘッダーに追加
-	client := new(http.Client)
-	resp, _ := client.Do(req)
-	defer resp.Body.Close()
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.New("").Delims("[[", "]]").ParseGlob("views/*.html")), // vue.jsとdelimsがかぶるので変更
+	}
+	e.Renderer = renderer
 
-	byteArray, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(byteArray))
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `"time":"${time_rfc3339}","remote_ip":"${remote_ip}","host":"${host}",` +
+			`"method":"${method}","uri":"${uri}","status":${status},"error":"${error}"` + "\n",
+	}))
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c echo.Context) error {
+		// 取得したいデータのURL作成
+		url, _ := url.Parse(config.Config.FootballDataBaseUrl)
+		url.Path = path.Join(url.Path, "teams", "108")
+
+		req, _ := http.NewRequest("GET", url.String(), nil)
+		req.Header.Add("X-Auth-Token", config.Config.FootballDataApiToken) // アカウント登録時に送られてきたAPIトークンをリクエストヘッダーに追加
+		client := new(http.Client)
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+
+		byteArray, _ := ioutil.ReadAll(resp.Body)
+		//fmt.Println(string(byteArray))
+		return c.JSON(http.StatusOK, string(byteArray))
+	})
+
 }
