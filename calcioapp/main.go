2a398c7c4439faa4d6e48c33daab5a400d84f610
nero-15 <nero15.dev@gmail.com>
2022-01-08 22:47:47 +0900

    predictions

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -415,5 +415,24 @@ func main() {
 		return c.String(http.StatusOK, string(byteArray))
 	})
 
+	e.GET("/api/predictions", func(c echo.Context) error {
+		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
+		url.Path = path.Join(url.Path, "predictions")
+
+		queryParams := url.Query()
+		queryParams.Set("season", "2021")
+		queryParams.Set("team", "505")
+		url.RawQuery = queryParams.Encode()
+
+		req, _ := http.NewRequest("GET", url.String(), nil)
+		req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
+		client := new(http.Client)
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+
+		byteArray, _ := ioutil.ReadAll(resp.Body)
+		return c.String(http.StatusOK, string(byteArray))
+	})
+
 	e.Logger.Fatal(e.Start(":8080"))
 }
