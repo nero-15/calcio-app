9a2f8c4008d654ac2b8b6075e1e3311599ec4303
nero-15 <nero15.dev@gmail.com>
2022-01-10 15:10:49 +0900

    fix

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -587,8 +587,7 @@ func main() {
 		url.Path = path.Join(url.Path, "players", "transfers")
 
 		queryParams := url.Query()
-		queryParams.Set("league", "135")
-		queryParams.Set("season", "2021")
+		queryParams.Set("team", "505")
 		url.RawQuery = queryParams.Encode()
 
 		req, _ := http.NewRequest("GET", url.String(), nil)
