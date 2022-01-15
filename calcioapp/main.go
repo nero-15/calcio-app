390a0dff9346f2ec46bbab95d3463ef846e605ca
nero-15 <nero15.dev@gmail.com>
2022-01-15 21:47:08 +0900

    fix

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -181,7 +181,7 @@ func main() {
 		return c.String(http.StatusOK, string(byteArray))
 	})
 
-	e.GET("/api/standings/SerieA", func(c echo.Context) error {
+	e.GET("api/apiFootball/league/:leagueId/standings", func(c echo.Context) error {
 		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
 		url.Path = path.Join(url.Path, "standings")
 
