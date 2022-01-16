5056f589ee41bb645d51008b4e7fa7ae1afdf880
nero-15 <nero15.dev@gmail.com>
2022-01-16 22:05:18 +0900

    fixture detail

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -161,6 +161,54 @@ func main() {
 		return c.String(http.StatusOK, string(byteArray))
 	})
 
+	e.GET("/api/apiFootball/league/:leagueId/team/:teamId/fixtures", func(c echo.Context) error {
+		leagueId := c.Param("leagueId")
+		teamId := c.Param("teamId")
+
+		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
+		url.Path = path.Join(url.Path, "fixtures")
+
+		queryParams := url.Query()
+		queryParams.Set("league", leagueId)
+		queryParams.Set("team", teamId)
+		queryParams.Set("season", "2021")
+		url.RawQuery = queryParams.Encode()
+
+		req, _ := http.NewRequest("GET", url.String(), nil)
+		req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
+		client := new(http.Client)
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+
+		byteArray, _ := ioutil.ReadAll(resp.Body)
+		return c.String(http.StatusOK, string(byteArray))
+	})
+
+	e.GET("/api/apiFootball/league/:leagueId/team/:teamId/fixture/:fixtureId", func(c echo.Context) error {
+		leagueId := c.Param("leagueId")
+		teamId := c.Param("teamId")
+		fixtureId := c.Param("fixtureId")
+
+		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
+		url.Path = path.Join(url.Path, "fixtures")
+
+		queryParams := url.Query()
+		queryParams.Set("id", fixtureId)
+		queryParams.Set("league", leagueId)
+		queryParams.Set("team", teamId)
+		queryParams.Set("season", "2021")
+		url.RawQuery = queryParams.Encode()
+
+		req, _ := http.NewRequest("GET", url.String(), nil)
+		req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
+		client := new(http.Client)
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+
+		byteArray, _ := ioutil.ReadAll(resp.Body)
+		return c.String(http.StatusOK, string(byteArray))
+	})
+
 	e.GET("/api/apiFootball/venues", func(c echo.Context) error {
 		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
 		url.Path = path.Join(url.Path, "venues")
@@ -188,29 +236,6 @@ func main() {
 		queryParams := url.Query()
 		queryParams.Set("id", venueId)
 		queryParams.Set("country", "Italy")
-		url.RawQuery = queryParams.Encode()
-
-		req, _ := http.NewRequest("GET", url.String(), nil)
-		req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
-		client := new(http.Client)
-		resp, _ := client.Do(req)
-		defer resp.Body.Close()
-
-		byteArray, _ := ioutil.ReadAll(resp.Body)
-		return c.String(http.StatusOK, string(byteArray))
-	})
-
-	e.GET("/api/apiFootball/league/:leagueId/team/:teamId/fixtures", func(c echo.Context) error {
-		leagueId := c.Param("leagueId")
-		teamId := c.Param("teamId")
-
-		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
-		url.Path = path.Join(url.Path, "fixtures")
-
-		queryParams := url.Query()
-		queryParams.Set("league", leagueId)
-		queryParams.Set("team", teamId)
-		queryParams.Set("season", "2021")
 		url.RawQuery = queryParams.Encode()
 
 		req, _ := http.NewRequest("GET", url.String(), nil)
