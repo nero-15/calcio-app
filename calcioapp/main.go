bae5a3603b958c8259b8e4a5af9d6d80930b4849
nero-15 <nero15.dev@gmail.com>
2022-01-19 21:36:05 +0900

    fix

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -407,25 +407,6 @@ func main() {
 		return c.String(http.StatusOK, string(byteArray))
 	})
 
-	e.GET("/api/injuries", func(c echo.Context) error {
-		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
-		url.Path = path.Join(url.Path, "injuries")
-
-		queryParams := url.Query()
-		queryParams.Set("season", "2021")
-		queryParams.Set("team", "505")
-		url.RawQuery = queryParams.Encode()
-
-		req, _ := http.NewRequest("GET", url.String(), nil)
-		req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
-		client := new(http.Client)
-		resp, _ := client.Do(req)
-		defer resp.Body.Close()
-
-		byteArray, _ := ioutil.ReadAll(resp.Body)
-		return c.String(http.StatusOK, string(byteArray))
-	})
-
 	e.GET("/api/predictions", func(c echo.Context) error {
 		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
 		url.Path = path.Join(url.Path, "predictions")
