92641faea6673e3a3825f1d935efe478619e3187
nero-15 <nero15.dev@gmail.com>
2022-01-28 21:19:43 +0900

    fix

--- calcioapp/apifootball/apifootball.go
+++ calcioapp/apifootball/apifootball.go
@@ -37,7 +37,6 @@ func (api *APIClient) doRequest(urlPath string, query map[string]string) (body s
 }
 
 func (api *APIClient) GetStatus() string {
-
 	url, _ := url.Parse(api.baseUrl)
 	url.Path = path.Join(url.Path, "status")
 
@@ -48,12 +47,27 @@ func (api *APIClient) GetStatus() string {
 	defer resp.Body.Close()
 
 	byteArray, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(byteArray))
-	fmt.Println(api.baseUrl)
-	fmt.Println(api.token)
 	return string(byteArray)
-
-	// body, _ := api.doRequest("status", map[string]string{})
-	// fmt.Println(body)
-	// return body
 }
+
+// func (api *APIClient) GetStatus() string {
+
+// 	url, _ := url.Parse(api.baseUrl)
+// 	url.Path = path.Join(url.Path, "status")
+
+// 	req, _ := http.NewRequest("GET", url.String(), nil)
+// 	req.Header.Add("x-apisports-key", api.token)
+// 	client := new(http.Client)
+// 	resp, _ := client.Do(req)
+// 	defer resp.Body.Close()
+
+// 	byteArray, _ := ioutil.ReadAll(resp.Body)
+// 	fmt.Println(string(byteArray))
+// 	fmt.Println(api.baseUrl)
+// 	fmt.Println(api.token)
+// 	return string(byteArray)
+
+// 	// body, _ := api.doRequest("status", map[string]string{})
+// 	// fmt.Println(body)
+// 	// return body
+// }
