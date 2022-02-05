c2a35490ba16c60cc6629b17e9ed3c2c22f4f941
nero-15 <nero15.dev@gmail.com>
2022-02-05 21:02:48 +0900

    fix

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -100,6 +100,9 @@ func main() {
 		if err != nil {
 			return echo.NewHTTPError(http.StatusNotFound, "not found")
 		}
+		if league.Results == 0 {
+			return echo.NewHTTPError(http.StatusNotFound, "not found")
+		}
 		leagueByteArray, _ := json.Marshal(league)
 		return c.String(http.StatusOK, string(leagueByteArray))
 	})
