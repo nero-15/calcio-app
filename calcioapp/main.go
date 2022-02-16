70f71f4a4b1a21a676c665ae3adcf7fd3b8e8999
nero-15 <nero15.dev@gmail.com>
2022-02-16 19:46:42 +0900

    fix

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -209,24 +209,26 @@ func main() {
 	e.GET("/api/apiFootball/league/:leagueId/team/:teamId/players", func(c echo.Context) error {
 		leagueId := c.Param("leagueId")
 		teamId := c.Param("teamId")
+		resp, _ := apifootball.GetStatisticsByLeagueIdAndTeamId(leagueId, teamId)
+		return c.String(http.
 
-		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
-		url.Path = path.Join(url.Path, "players")
+		// url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
+		// url.Path = path.Join(url.Path, "players")
 
-		queryParams := url.Query()
-		queryParams.Set("league", leagueId)
-		queryParams.Set("team", teamId)
-		queryParams.Set("season", "2021")
-		url.RawQuery = queryParams.Encode()
+		// queryParams := url.Query()
+		// queryParams.Set("league", leagueId)
+		// queryParams.Set("team", teamId)
+		// queryParams.Set("season", "2021")
+		// url.RawQuery = queryParams.Encode()
 
-		req, _ := http.NewRequest("GET", url.String(), nil)
-		req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
-		client := new(http.Client)
-		resp, _ := client.Do(req)
-		defer resp.Body.Close()
+		// req, _ := http.NewRequest("GET", url.String(), nil)
+		// req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
+		// client := new(http.Client)
+		// resp, _ := client.Do(req)
+		// defer resp.Body.Close()
 
-		byteArray, _ := ioutil.ReadAll(resp.Body)
-		return c.String(http.StatusOK, string(byteArray))
+		// byteArray, _ := ioutil.ReadAll(resp.Body)
+		// return c.String(http.StatusOK, string(byteArray))
 	})
 
 	e.GET("/api/apiFootball/league/:leagueId/team/:teamId/fixtures", func(c echo.Context) error {
