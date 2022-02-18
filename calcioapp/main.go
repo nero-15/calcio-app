7517eca141d93a3720a0c2fc5537cf6a4982aee3
nero-15 <nero15.dev@gmail.com>
2022-02-19 08:01:31 +0900

    fix

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -270,21 +270,8 @@ func main() {
 	e.GET("/api/apiFootball/team/:teamId/coachs", func(c echo.Context) error {
 		teamId := c.Param("teamId")
 
-		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
-		url.Path = path.Join(url.Path, "coachs")
-
-		queryParams := url.Query()
-		queryParams.Set("team", teamId)
-		url.RawQuery = queryParams.Encode()
-
-		req, _ := http.NewRequest("GET", url.String(), nil)
-		req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
-		client := new(http.Client)
-		resp, _ := client.Do(req)
-		defer resp.Body.Close()
-
-		byteArray, _ := ioutil.ReadAll(resp.Body)
-		return c.String(http.StatusOK, string(byteArray))
+		resp, _ := apifootball.GetCoachsByTeamId(teamId)
+		return c.String(http.StatusOK, string(resp))
 	})
 
 	e.GET("/api/apiFootball/team/:teamId/squads", func(c echo.Context) error {
