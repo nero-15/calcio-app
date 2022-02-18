b465d2e409731034c4eb06e5971b9b3e4a52f056
nero-15 <nero15.dev@gmail.com>
2022-02-18 21:48:18 +0900

    fix

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -247,22 +247,8 @@ func main() {
 		teamId := c.Param("teamId")
 		fixtureId := c.Param("fixtureId") //731698
 
-		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
-		url.Path = path.Join(url.Path, "fixtures", "events")
-
-		queryParams := url.Query()
-		queryParams.Set("fixture", fixtureId)
-		queryParams.Set("team", teamId)
-		url.RawQuery = queryParams.Encode()
-
-		req, _ := http.NewRequest("GET", url.String(), nil)
-		req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
-		client := new(http.Client)
-		resp, _ := client.Do(req)
-		defer resp.Body.Close()
-
-		byteArray, _ := ioutil.ReadAll(resp.Body)
-		return c.String(http.StatusOK, string(byteArray))
+		resp, _ := apifootball.GetEventsByTeamIdAndFixtureId(teamId, fixtureId)
+		return c.String(http.StatusOK, string(resp))
 	})
 
 	e.GET("/api/apiFootball/team/:teamId/fixture/:fixtureId/lineups", func(c echo.Context) error {
