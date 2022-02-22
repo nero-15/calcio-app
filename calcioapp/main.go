fb6a9c8adff5316c98f683f089329b60e6443f45
nero-15 <nero15.dev@gmail.com>
2022-02-22 22:00:31 +0900

    fix

--- calcioapp/main.go
+++ calcioapp/main.go
@@ -306,22 +306,8 @@ func main() {
 	e.GET("/api/apiFootball/player/:playerId", func(c echo.Context) error {
 		playerId := c.Param("playerId") // M. Škriniar: 198
 
-		url, _ := url.Parse(config.Config.ApiFootballBaseUrl)
-		url.Path = path.Join(url.Path, "players")
-
-		queryParams := url.Query()
-		queryParams.Set("id", playerId)
-		queryParams.Set("season", "2021")
-		url.RawQuery = queryParams.Encode()
-
-		req, _ := http.NewRequest("GET", url.String(), nil)
-		req.Header.Add("x-apisports-key", config.Config.ApiFootballApiToken)
-		client := new(http.Client)
-		resp, _ := client.Do(req)
-		defer resp.Body.Close()
-
-		byteArray, _ := ioutil.ReadAll(resp.Body)
-		return c.String(http.StatusOK, string(byteArray))
+		resp, _ := apifootball.GetPlayersByPlayerId(playerId)
+		return c.String(http.StatusOK, string(resp))
 	})
 
 	e.GET("/api/apiFootball/player/:playerId/transfers", func(c echo.Context) error {
