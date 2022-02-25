88c0f60da8137ef223a30763103d3a843510e270
nero-15 <nero15.dev@gmail.com>
2022-02-25 22:46:23 +0900

    add Statistics

--- calcioapp/apifootball/apifootball.go
+++ calcioapp/apifootball/apifootball.go
@@ -453,6 +453,273 @@ func (api *APIClient) GetTeamsByLeagueIdAndTeamId(leagueId string, teamId string
 	return teams, nil
 }
 
+type Statistics struct {
+	Get        string `json:"get"`
+	Parameters struct {
+		League string `json:"league"`
+		Season string `json:"season"`
+		Team   string `json:"team"`
+	} `json:"parameters"`
+	Errors  []interface{} `json:"errors"`
+	Results int           `json:"results"`
+	Paging  struct {
+		Current int `json:"current"`
+		Total   int `json:"total"`
+	} `json:"paging"`
+	Response struct {
+		League struct {
+			ID      int    `json:"id"`
+			Name    string `json:"name"`
+			Country string `json:"country"`
+			Logo    string `json:"logo"`
+			Flag    string `json:"flag"`
+			Season  int    `json:"season"`
+		} `json:"league"`
+		Team struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+			Logo string `json:"logo"`
+		} `json:"team"`
+		Form     string `json:"form"`
+		Fixtures struct {
+			Played struct {
+				Home  int `json:"home"`
+				Away  int `json:"away"`
+				Total int `json:"total"`
+			} `json:"played"`
+			Wins struct {
+				Home  int `json:"home"`
+				Away  int `json:"away"`
+				Total int `json:"total"`
+			} `json:"wins"`
+			Draws struct {
+				Home  int `json:"home"`
+				Away  int `json:"away"`
+				Total int `json:"total"`
+			} `json:"draws"`
+			Loses struct {
+				Home  int `json:"home"`
+				Away  int `json:"away"`
+				Total int `json:"total"`
+			} `json:"loses"`
+		} `json:"fixtures"`
+		Goals struct {
+			For struct {
+				Total struct {
+					Home  int `json:"home"`
+					Away  int `json:"away"`
+					Total int `json:"total"`
+				} `json:"total"`
+				Average struct {
+					Home  string `json:"home"`
+					Away  string `json:"away"`
+					Total string `json:"total"`
+				} `json:"average"`
+				Minute struct {
+					Zero15 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"0-15"`
+					One630 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"16-30"`
+					Three145 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"31-45"`
+					Four660 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"46-60"`
+					Six175 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"61-75"`
+					Seven690 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"76-90"`
+					Nine1105 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"91-105"`
+					One06120 struct {
+						Total      interface{} `json:"total"`
+						Percentage interface{} `json:"percentage"`
+					} `json:"106-120"`
+				} `json:"minute"`
+			} `json:"for"`
+			Against struct {
+				Total struct {
+					Home  int `json:"home"`
+					Away  int `json:"away"`
+					Total int `json:"total"`
+				} `json:"total"`
+				Average struct {
+					Home  string `json:"home"`
+					Away  string `json:"away"`
+					Total string `json:"total"`
+				} `json:"average"`
+				Minute struct {
+					Zero15 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"0-15"`
+					One630 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"16-30"`
+					Three145 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"31-45"`
+					Four660 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"46-60"`
+					Six175 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"61-75"`
+					Seven690 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"76-90"`
+					Nine1105 struct {
+						Total      int    `json:"total"`
+						Percentage string `json:"percentage"`
+					} `json:"91-105"`
+					One06120 struct {
+						Total      interface{} `json:"total"`
+						Percentage interface{} `json:"percentage"`
+					} `json:"106-120"`
+				} `json:"minute"`
+			} `json:"against"`
+		} `json:"goals"`
+		Biggest struct {
+			Streak struct {
+				Wins  int `json:"wins"`
+				Draws int `json:"draws"`
+				Loses int `json:"loses"`
+			} `json:"streak"`
+			Wins struct {
+				Home string `json:"home"`
+				Away string `json:"away"`
+			} `json:"wins"`
+			Loses struct {
+				Home string `json:"home"`
+				Away string `json:"away"`
+			} `json:"loses"`
+			Goals struct {
+				For struct {
+					Home int `json:"home"`
+					Away int `json:"away"`
+				} `json:"for"`
+				Against struct {
+					Home int `json:"home"`
+					Away int `json:"away"`
+				} `json:"against"`
+			} `json:"goals"`
+		} `json:"biggest"`
+		CleanSheet struct {
+			Home  int `json:"home"`
+			Away  int `json:"away"`
+			Total int `json:"total"`
+		} `json:"clean_sheet"`
+		FailedToScore struct {
+			Home  int `json:"home"`
+			Away  int `json:"away"`
+			Total int `json:"total"`
+		} `json:"failed_to_score"`
+		Penalty struct {
+			Scored struct {
+				Total      int    `json:"total"`
+				Percentage string `json:"percentage"`
+			} `json:"scored"`
+			Missed struct {
+				Total      int    `json:"total"`
+				Percentage string `json:"percentage"`
+			} `json:"missed"`
+			Total int `json:"total"`
+		} `json:"penalty"`
+		Lineups []struct {
+			Formation string `json:"formation"`
+			Played    int    `json:"played"`
+		} `json:"lineups"`
+		Cards struct {
+			Yellow struct {
+				Zero15 struct {
+					Total      int    `json:"total"`
+					Percentage string `json:"percentage"`
+				} `json:"0-15"`
+				One630 struct {
+					Total      int    `json:"total"`
+					Percentage string `json:"percentage"`
+				} `json:"16-30"`
+				Three145 struct {
+					Total      int    `json:"total"`
+					Percentage string `json:"percentage"`
+				} `json:"31-45"`
+				Four660 struct {
+					Total      int    `json:"total"`
+					Percentage string `json:"percentage"`
+				} `json:"46-60"`
+				Six175 struct {
+					Total      int    `json:"total"`
+					Percentage string `json:"percentage"`
+				} `json:"61-75"`
+				Seven690 struct {
+					Total      int    `json:"total"`
+					Percentage string `json:"percentage"`
+				} `json:"76-90"`
+				Nine1105 struct {
+					Total      int    `json:"total"`
+					Percentage string `json:"percentage"`
+				} `json:"91-105"`
+				One06120 struct {
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
+				} `json:"106-120"`
+			} `json:"yellow"`
+			Red struct {
+				Zero15 struct {
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
+				} `json:"0-15"`
+				One630 struct {
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
+				} `json:"16-30"`
+				Three145 struct {
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
+				} `json:"31-45"`
+				Four660 struct {
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
+				} `json:"46-60"`
+				Six175 struct {
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
+				} `json:"61-75"`
+				Seven690 struct {
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
+				} `json:"76-90"`
+				Nine1105 struct {
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
+				} `json:"91-105"`
+				One06120 struct {
+					Total      interface{} `json:"total"`
+					Percentage interface{} `json:"percentage"`
+				} `json:"106-120"`
+			} `json:"red"`
+		} `json:"cards"`
+	} `json:"response"`
+}
+
 func (api *APIClient) GetStatisticsByLeagueIdAndTeamId(leagueId string, teamId string) ([]byte, error) {
 	resp, err := api.doRequest("teams/statistics", map[string]string{
 		"season": "2021",
