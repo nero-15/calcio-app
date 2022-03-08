c5772b78e3e304a7f378a7e1f6bdf1818d3e23a0
nero-15 <nero15.dev@gmail.com>
2022-03-08 22:10:51 +0900

    add Events struct

--- calcioapp/apifootball/apifootball.go
+++ calcioapp/apifootball/apifootball.go
@@ -828,6 +828,42 @@ func (api *APIClient) GetEventsByTeamIdAndFixtureId(teamId string, fixtureId str
 	return resp, err
 }
 
+type Events struct {
+	Get        string `json:"get"`
+	Parameters struct {
+		Fixture string `json:"fixture"`
+		Team    string `json:"team"`
+	} `json:"parameters"`
+	Errors  []interface{} `json:"errors"`
+	Results int           `json:"results"`
+	Paging  struct {
+		Current int `json:"current"`
+		Total   int `json:"total"`
+	} `json:"paging"`
+	Response []struct {
+		Time struct {
+			Elapsed int         `json:"elapsed"`
+			Extra   interface{} `json:"extra"`
+		} `json:"time"`
+		Team struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+			Logo string `json:"logo"`
+		} `json:"team"`
+		Player struct {
+			ID   int    `json:"id"`
+			Name string `json:"name"`
+		} `json:"player"`
+		Assist struct {
+			ID   interface{} `json:"id"`
+			Name interface{} `json:"name"`
+		} `json:"assist"`
+		Type     string      `json:"type"`
+		Detail   string      `json:"detail"`
+		Comments interface{} `json:"comments"`
+	} `json:"response"`
+}
+
 func (api *APIClient) GetLineupsByTeamIdAndFixtureId(teamId string, fixtureId string) ([]byte, error) {
 	resp, err := api.doRequest("fixtures/lineups", map[string]string{
 		"team":    teamId,
