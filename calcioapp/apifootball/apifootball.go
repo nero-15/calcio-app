17df17f9918284bfbc6ef2abf2973c24f44dcbbc
nero-15 <nero15.dev@gmail.com>
2022-03-14 21:38:59 +0900

    fix

--- calcioapp/apifootball/apifootball.go
+++ calcioapp/apifootball/apifootball.go
@@ -307,6 +307,7 @@ type Venue struct {
 	Name     string `json:"name"`
 	Address  string `json:"address"`
 	City     string `json:"city"`
+	Country  string `json:"country"`
 	Capacity int    `json:"capacity"`
 	Surface  string `json:"surface"`
 	Image    string `json:"image"`
